8d4a3a806a77c79b8865717aae480bd43f42abc8
youngje <dldudwp333@gmail.com>
2023-09-18 14:43:14 +0900

    rename_py_file

--- pythonProject/mat4.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-figsize = (7,7)
-fig,ax = plt.subplots(figsize=figsize)
-
-fig.suptitle("Title of a Figure", fontsize =30, color = 'darkblue', alpha= 0.9)
-ax.set_xlabel("X label", fontsize = 20,color = 'darkblue', alpha= 0.7)
-ax.set_ylabel("Y label", fontsize = 20,color = 'darkblue', alpha= 0.7)
-
-fig.show()
